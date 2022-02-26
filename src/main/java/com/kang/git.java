0251da4619d52385a69e3f1e1db0fcee9b34994f
zzk <2687052072@qq.com>
2022-02-26 14:27:18 +0800

    third commit

--- src/main/java/com/kang/git.java
+++ src/main/java/com/kang/git.java
@@ -8,5 +8,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("gitgit");
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
